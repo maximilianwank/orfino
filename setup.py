83745612ba6b975935c7a2ffae0859f959d22af2
Maximilian Wank <maxi.wank@gmx.de>
2022-06-28 15:32:26 +0200

    fix double https in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="orfino",
     version="0.0.1",
-    url="https://https://github.com/maximilianwank/orfino",
+    url="https://github.com/maximilianwank/orfino",
     author="Maximilian Wank",
     author_email="orfino@alpenjodel.de",
     description="Send notifications for filled orders on crypto exchanges",
