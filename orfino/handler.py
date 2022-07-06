e923038f1bd58e80d8f01613acc138c3924f3481
Maximilian Wank <maxi.wank@gmx.de>
2022-07-06 10:30:08 +0200

    explicitly state whether to log exceptions of notify my device function and set it to false in handler emit

--- orfino/handler.py
+++ orfino/handler.py
@@ -10,13 +10,15 @@ logger = logging.getLogger(__name__)
 
 
 def send_notify_my_device_message(
-    api_key: str, title: str, body: str
+    api_key: str, title: str, body: str, log_url_errors: bool
 ) -> typing.Optional[http.client.HTTPResponse]:
     """
     Send a message through the Notify My Device service to your smartphone
     :param api_key: API key generated on notifymydevice.com/myapplications
     :param title: Title of the message (encoding utf-8)
     :param body: Body of the message (encoding utf-8)
+    :param log_url_errors: Decide whether to log URLErrors or not. Must be set to
+    false if used in emit method of some log handler to avoid RecursionError
     :return: Response of
     """
     data = json.dumps({"ApiKey": api_key, "PushTitle": title, "PushText": body})
@@ -28,7 +30,8 @@ def send_notify_my_device_message(
     try:
         response = urllib.request.urlopen(req)
     except urllib.error.URLError as e:
-        logger.warning(f"Unable to send notification via Notify My Device: {e}")
+        if log_url_errors:
+            logger.warning(f"Unable to send notification via Notify My Device: {e}")
         response = None
     return response
 
@@ -57,4 +60,5 @@ class NotifyMyDeviceHandler(logging.Handler):
             api_key=self.api_key,
             title=f"{symbol} {record.filename}",
             body=self.format(record=record),
+            log_url_errors=False,
         )
