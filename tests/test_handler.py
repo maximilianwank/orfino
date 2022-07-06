2afbec01caab1f714e3dd4e073e74bd757a39dd6
Maximilian Wank <maxi.wank@gmx.de>
2022-07-06 11:07:45 +0200

    comments in testcase

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -12,7 +12,10 @@ class TestNotifyMyDeviceHandler(TestCase):
 
     @patch("urllib.request.urlopen")
     def test_no_logging_of_url_errors(self, mock_urllib_request_urlopen):
+        # let urllib raise an URLError when trying to send notify my device request
         mock_urllib_request_urlopen.side_effect = urllib.error.URLError("bar")
+        # create sample log record for NotifyMyDeviceHandler.emit call
         log_record = logging.makeLogRecord(dict={"levelno": logging.ERROR})
+        # check that during emit of log record no new log messages are created
         with self.assertNoLogs():
             self.handler.emit(log_record)
